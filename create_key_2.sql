29c7a0fbb2eb957b763934b6a75d902a645f84f7
nevermore6264 <nevermore6264@gmail.com>
2018-05-08 10:52:55 +0700

    foreign key

---
+++ create_key_2.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE create_key_2;
+
+USE create_key_2;
+
+CREATE TABLE customers(
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(40),
+   address VARCHAR(255),
+   email VARCHAR(255)
+);
+
+CREATE TABLE orders(
+   id INT AUTO_INCREMENT,
+   staff VARCHAR(50),
+   PRIMARY KEY(id),
+    customer_id INT,
+   FOREIGN KEY (customer_id) REFERENCES customers(id)
+);
